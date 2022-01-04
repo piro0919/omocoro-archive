fb879cc02ad8ff85770b5be148c4dfd80197d2d2
piro <piro.haniwa@gmail.com>
2022-01-04 19:59:25 +0900

    feat: add next-pwa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
+const withPWA = require("next-pwa");
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+module.exports = withPWA({
   images: {
     domains: ["omocoro.jp"],
   },
+  pwa: {
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+  },
   reactStrictMode: true,
-};
+});
