6089a22799deccfc9fefd152e3686e8bce2d0197
piro <piro.haniwa@gmail.com>
2022-01-05 21:38:18 +0900

    feat: add manifest.json

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,24 @@ import "../styles/globals.scss";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import "ress";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 if (process.env.NODE_ENV === "development") {
   require("../styles/show-breakpoints.scss");
 }
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+          name="viewport"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
